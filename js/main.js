9b732e993de664611d50d85e435eed7bb1a44c2f
Matthew Raymer <matthew.raymer@anomalistdesign.com>
2016-03-20 12:12:07 +0800

    updates

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
 
+  var main_init = function() {
+    var   source = $("#step-01-template").html();
+    var template = Handlebars.compile( source );
+    var  context = { title: "Paste Article", description: "Paste an existing article in the textbox below."};
+    var     html = template(context);
+  };
+
+  $(document).ready( main_init );
