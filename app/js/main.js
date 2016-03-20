4e5edc26cba04c85a6081a893b70f73b687f6919
Matthew Raymer <matthew.raymer@anomalistdesign.com>
2016-03-20 19:58:16 +0800

    adding form hook

--- app/js/main.js
+++ app/js/main.js
@@ -3,9 +3,11 @@
     var text = $('#existing_article').val().trim();
     var result = text.match( /[^\.!\?]+[\.!\?]+/g );
     console.log( result );
+    return false;
   };
 
   var Step01 = function() {
+    $('#formStep01').submit( function() { return false; });
     var   source = $("#step-01-template").html();
     var template = Handlebars.compile( source );
     var  context = { title: "Paste Article", description: "Paste an existing article in the textbox below."};
