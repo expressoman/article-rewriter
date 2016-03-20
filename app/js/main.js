c37c347de55b286e46b38d6c393a4da79999d285
Matthew R. Raymer <matthew.raymer@anomalistdesign.comsudo>
2016-03-20 05:28:37 +0000

    step 1

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,20 @@
 
-  var main_init = function() {
+  var btnParse_click = function() {};
+  var btnPaste_click = function() {};
+
+  var Step01 = function() {
     var   source = $("#step-01-template").html();
     var template = Handlebars.compile( source );
     var  context = { title: "Paste Article", description: "Paste an existing article in the textbox below."};
-    var     html = template(context);
+    var     html = template( context );
+    $("#content").html( html );
+
+    $("#btnPaste").click( btnPaste_click );
+    $("#btnParse").click( btnParse_click );
+  };
+
+  var main_init = function() {
+    Step01();
   };
 
   $(document).ready( main_init );
